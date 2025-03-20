14acb7574088f9e28edd2220e721e9ff61bf6769
░L░E░O░ <83369389+devmailar@users.noreply.github.com>
2025-03-20 20:14:44 +0200

    First spectate camera concept

--- Client/Main.cs
+++ Client/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using CitizenFX.Core;
 using CitizenFX.Core.Native;
 
@@ -15,7 +16,29 @@ namespace RoundManager.Client
         {
             Debug.WriteLine("Entering spectator mode.");
 
-            API.NetworkSetInSpectatorMode(true, Game.Player.Handle);
+            _ = Spectate();
+        }
+
+
+        private async Task Spectate()
+        {
+            await Delay(60000);
+
+            API.NetworkResurrectLocalPlayer(248.86f, 207.23f, 106.96f, 22.34f, false, false);
+
+            Debug.WriteLine("NetworkResurrectLocalPlayer");
+
+            var camera = API.CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", 248.86f, 207.23f, 106.96f, 0f, 0f, 0f, 40f, true, (int)Game.Player.Character.Rotation.Z);
+
+            Debug.WriteLine("CreateCamWithParams");
+
+            API.RenderScriptCams(true, true, 2000, false, false);
+
+            Debug.WriteLine("RenderScriptCams");
+
+            API.SetCamActive(camera, true);
+
+            Debug.WriteLine("SetCamActive");
         }
     }
 }
