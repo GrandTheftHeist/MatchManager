2b33024a1d0836aabb132650a91fbfea5c6a04c6
░L░E░O░ <83369389+devmailar@users.noreply.github.com>
2025-04-06 16:37:23 +0300

    ServerMain.cs with business logic

--- Server/ServerMain.cs
+++ Server/ServerMain.cs
@@ -1,17 +1,98 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using CitizenFX.Core;
+using CitizenFX.Core.Native;
 
 namespace RoundManager.Server
 {
-    internal class ServerMain : BaseScript
+    public class ServerMain : BaseScript
     {
-        private readonly Round round;
-
         public ServerMain()
         {
-            round = new Round(Guid.NewGuid(), DateTime.Now, DateTime.Now.AddSeconds(1));
+            Round.Create(Guid.NewGuid(), DateTime.Now.AddMinutes(1), DateTime.Now.AddSeconds(1));
+        }
 
-            Tick += round.RoundTick;
+        [Tick]
+        public async Task OnTick()
+        {
+            await Delay(1000);
+
+            if (Players.Count() == 0)
+            {
+                Debug.WriteLine($"^3[WARNING] No Players online.^7");
+                return;
+            }
+
+            if (DateTime.Now < Round.EndTime)
+            {
+                Debug.WriteLine($"^6[DEBUG] DateTime.Now < Round.EndTime...^7");
+
+                foreach (var player in Players)
+                {
+                    if (player.State.Get("isPlaying") == true)
+                    {
+                        Debug.WriteLine($"^6[DEBUG] {player.Name} is playing {Round.StartTime} - {Round.EndTime}^7");
+
+                        continue;
+                    }
+
+                    if (player.State.Get("isSpectating") == true)
+                    {
+                        Debug.WriteLine($"^6[DEBUG] {player.Name} is spectating {Round.StartTime} - {Round.EndTime}^7");
+
+                        player.State.Set("endTime", Round.EndTime, true);
+
+                        continue;
+                    }
+
+                    if (player.State.Get("isPlaying") != true && player.State.Get("isSpectating") != true)
+                    {
+                        Debug.WriteLine($"^6[DEBUG] {player.Name} is now spectating {Round.StartTime} - {Round.EndTime}^7");
+
+                        player.State.Set("isPlaying", false, true);
+                        player.State.Set("isSpectating", true, true);
+
+                        player.TriggerEvent("CORE_CL_ROUND_SPECTATE");
+                    }
+                }
+
+                return;
+            }
+
+            if (DateTime.Now >= Round.EndTime)
+            {
+                Debug.WriteLine($"^6[DEBUG] {Round.EndTime}");
+                Debug.WriteLine($"^5[INFO] Round '{Round.Id}' ended.^7");
+                Debug.WriteLine($"^5[INFO] Creating next round...^7");
+
+                foreach (uint ped in API.GetAllPeds())
+                {
+                    if (!API.IsPedAPlayer((int)ped))
+                    {
+                        continue;
+                    }
+
+                    API.DeleteEntity((int)ped);
+                }
+
+                TriggerEvent("BANK_SV_RESTART_BY_SERVER");
+                TriggerEvent("BANK_SV_DOOR_CLOSE_BY_SERVER", 0, 1);
+                TriggerEvent("BANK_SV_DOOR_CLOSE_BY_SERVER", 0, 2);
+                TriggerEvent("BANK_SV_DOOR_CLOSE_BY_SERVER", 0, 3);
+
+                Round.Create(Guid.NewGuid(), DateTime.Now, DateTime.Now.AddMinutes(10));
+
+                foreach (var player in Players)
+                {
+                    player.State.Set("endTime", Round.EndTime, true);
+
+                    player.State.Set("isPlaying", true, true);
+                    player.State.Set("isSpectating", false, true);
+
+                    player.TriggerEvent("CORE_CL_ROUND_STARTED");
+                }
+            }
         }
     }
 }
