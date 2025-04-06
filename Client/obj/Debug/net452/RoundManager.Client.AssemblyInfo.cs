c66b29fee23abaeef7d44f27b7404a5e0d67b3b3
░L░E░O░ <83369389+devmailar@users.noreply.github.com>
2025-04-06 16:37:26 +0300

    Other

--- Client/obj/Debug/net452/RoundManager.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net452/RoundManager.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoundManager.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9189d5fc66589173903d4f7d2a9ff8fb4727bfe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc58ece79adc1a0a2d657a7fd8993bdb2a3e9f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoundManager.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoundManager.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
